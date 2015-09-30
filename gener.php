35c51611e3e006536b4fb061b10ee68010a1876c
Vladislavvitalevich <vladislavvitalevich@gmail.com>
2015-09-30 23:55:34 +0300

    clean_gener

--- gener.php
+++ gener.php
@@ -7,20 +7,10 @@ $firstPartUrl = $url["host"]; //1 часть url
 //забираем последний элемент массива
 $secondPartUrl = array_pop($url); //2 часть url
 
-//создание короткой ссылки
-function generateLink($length = 4)
-{
-    $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
-    $shortUrl = '';
-    for ($i = 0; $i < $length; $i++) {
-        $shortUrl .= substr($chars, rand(0, strlen($chars) - 1), 1);
-    }
-
-    return $shortUrl;
-}
 
 $shortUrl = substr(uniqid(), -6); //3 запись в бд
-var_dump($shortUrl); die;
+var_dump($shortUrl);
+die;
 
 //подключение к бд
 //db config
@@ -28,12 +18,6 @@ $host = 'localhost';
 $database = 'short_link';
 $login = 'root';
 $password = '';
-
-//try {
-//    $db = new PDO('mysql:host=' . $host . ';db=' . $database, $login, $password);
-//} catch (PDOException $e) {
-//    echo $e->getMessage();
-//}
 
 $db = new PDO('mysql:host=localhost;dbname=short_link', 'root', '');
 $sql = "INSERT INTO address (domain, url, short_url) VALUES (:firstPart, :secondPart, :short)";
@@ -46,6 +30,5 @@ $stmt->execute(
         ':short' => $shortUrl
     )
 );
-//"INSERT INTO" . " $table "."(domain, url, short_url) VALUES ('" .$firstPartUrl."', '" .$secondPartUrl. "', '" .$shortUrl."')";
-//$result = $db->exec($stmt);
+
 
